0414c61449a38c6494197a33eacffddbfb4a82f1
roylaurie <roy.laurie.pub@sourcetrait.com>
2025-12-11 22:25:35 -0800

    wip formula

--- Formula/sourcetrait_rpg.rb
+++ Formula/sourcetrait_rpg.rb
@@ -1,7 +1,7 @@
 class SourcetraitRpg < Formula
   desc "PostgreSQL bindings for Rust"
   homepage "https://sourcetrait.com"
-  url "https://github.com/sourcetrait/homebrew-tap/releases/download/sourcetrait_rpg%2F0.0.1/sourcetrait_rpg_pg18.tar.gz"
+  url "https://github.com/sourcetrait/homebrew-tap/releases/download/sourcetrait_rpg_v0.0.1/sourcetrait_rpg_0.0.1.tar.gz"
   sha256 "8d7317bb22b84898fd0cc4e2c6842d10e710bf34573dde3bd45a48636b1d57db"
   license "AGPL-3"
 
